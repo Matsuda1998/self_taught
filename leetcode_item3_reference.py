465b9890b5adfce21344cd98d90009f90b6da761
Matsuda1998 <matsuda@inspiron16.localdomain>
2022-11-23 10:45:45 +0900

    reference vol.1

---
+++ leetcode_item3_reference.py
@@ -0,0 +1,16 @@
+# Sample 46 ms submission
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        j = -1 #初期値が-1なのはインデックス0の要素が最初に出て来た時ansを"1"にするため
+        chars = {}
+        ans = 0
+        for i,ch in enumerate(s):
+            if ch in chars and chars[ch] > j:
+                j = chars[ch] # jをその文字要素が前回出て来たインデックスに合わせる
+            else:
+                if ans < i-j: #要素と要素の間隔が従来の記録を上回れば
+                    ans = i-j # ansを更新
+            chars[ch] = i # 文字の要素のインデックスを保存
+        return ans
+
+Solution().lengthOfLongestSubstring("dvdf")
