1aadda9bcbd66091b9c26d4668aeb6aa2b6654fa
Matsuda1998 <matsuda@inspiron16.localdomain>
2022-12-15 15:43:20 +0900

    リストを文字に直す。ロジックはＯＫ

--- leetcode_item6.py
+++ leetcode_item6.py
@@ -2,49 +2,28 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s 
+        mtrx =[]
+        for i in range(numRows):
+            mtrx.append([]) # mtrx = [[]]*numRowsは入れ子の[]が同じオブジェクトになるのでダメ
+        j=0
+        for val in s:            
+            mtrx[j].append(val)
+            if j == 0 :
+                flag = 0
+            if j < numRows-1 and flag == 0 :
+                j += 1
+            else:
+                j -= 1
+                flag = 1
+        return mtrx #リストを文字に直す。ロジックはＯＫ
 
+Solution().convert("PAYPALISHIRING",4)
 
-
-"""
-s[i]の位置を求める。
-s="PAYPALISHIRING"
-rows=3
-mat_list=[[]]*rows
-[0 4 8 2
- 1357913
- 2 6 0]
-raws=2
-[0246802
- 1357913]
-rows=4
-[0  6  2
- 1 57 13
- 24 80
- 3  9   ]
-rows=5
-[0   8   6  
- 1  79  57
- 2 6 0 4 8
- 35  13  9     s[3]=mat_list[3][0],s[5]=mat_list[3][1],s[11]=mat_list[3][3]
- 4   2   0]
-rows=6
-[0    0
- 1   91
- 2  8 2
- 3 7  3
- 46   4
- 5    5
-ros=7
-[0     2
- 1    1
- 2   0
- 3  9
- 4 8
- 57
- 6
 
 
 
 
 """
-[[]]*4
+vectorworksで検討する(zigzag_conversion検討.vwx)列方向yのスペースは無視して詰めてＯＫ)
+"""
+
